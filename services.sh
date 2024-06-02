c2bda189d310783ddda21f6ae6c3ce87cdebc6c0
Ondrej Brablc <ondrej@brablc.com>
2024-06-02 19:02:39 +0200

    Add check, that runs on manager node

--- services.sh
+++ services.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source ./config.sh
 source ./logger.sh
 
 sock=/var/run/docker.sock
@@ -7,7 +8,11 @@ url=http://v1.45
 
 LABEL="swarm-health-alerter.port"
 
-curl -s --unix-socket $sock $url/services -o /tmp/services
+curl -s --fail-with-body --unix-socket $sock $url/servicess -o /tmp/services
+if [ $? -ne 0 ]; then
+    log_error "$(jq -r .message /tmp/services 2>/dev/null || cat /tmp/services)"
+    exit 1
+fi
 
 function get_services() {
     cat /tmp/services | jq -r '.[] | select(.Spec.Labels["com.docker.stack.namespace"] != null) | .Spec.Name' | sort -u
