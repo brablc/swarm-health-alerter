a1ec360a01e2ae21696ae7142a1d64a8987d8bbf
Ondrej Brablc <ondrej@brablc.com>
2024-06-03 21:44:51 +0200

    Switch starting logic

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -4,17 +4,15 @@ source "./config.sh"
 source "./logger.sh"
 source "./checks.sh"
 
-log_info "Starting event alerter ..."
-./event-alerter.py &
-trap "kill $!" EXIT
-
 services=$(./services.sh 2>&1)
-if [ $? != 0 ]; then
-    exit
+if [ $? = 0 ]; then
+    log_info "Initial list of services:"
+    echo "$services"
+
+    log_info "Starting port alerter ..."
+    ./port-alerter.sh &
+    trap "kill $!" EXIT
 fi
 
-log_info "Initial list of services:"
-echo "$services"
-
-log_info "Starting port alerter ..."
-./port-alerter.sh
+log_info "Starting event alerter ..."
+./event-alerter.py
